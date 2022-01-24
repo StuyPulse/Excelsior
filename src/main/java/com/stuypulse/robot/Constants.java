04a0bbd542a27910da779b8b3f57496285d08f77
Myles Pasetsky <mp3blue99@gmail.com>
2022-01-24 18:45:12 -0500

    Port over Edwin drivetrain and constants

--- src/main/java/com/stuypulse/robot/Constants.java
+++ src/main/java/com/stuypulse/robot/Constants.java
@@ -6,7 +6,14 @@
 package com.stuypulse.robot;
 
 import com.stuypulse.stuylib.network.SmartBoolean;
+import com.stuypulse.stuylib.network.SmartNumber;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import java.nio.file.Path;
 
@@ -18,7 +25,7 @@ import java.nio.file.Path;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public interface Constants {
 
     Path DEPLOY_DIRECTORY = Filesystem.getDeployDirectory().toPath();
 
@@ -33,18 +40,15 @@ public final class Constants {
         }
 
         public interface Drivetrain {
-            int LEFT_TOP = 7;
-            int LEFT_BOTTOM = 6;
+            int LEFT_TOP = -1;
+            int LEFT_MIDDLE = -1;
+            int LEFT_BOTTOM = -1;
 
-            int RIGHT_TOP = 4;
-            int RIGHT_BOTTOM = 3;
+            int RIGHT_TOP = -1;
+            int RIGHT_MIDDLE = -1;
+            int RIGHT_BOTTOM = -1;
 
-            int LEFT_ENCODER_A = 0;
-            int LEFT_ENCODER_B = 1;
-            int RIGHT_ENCODER_A = 2;
-            int RIGHT_ENCODER_B = 3;
-
-            int GEAR_SHIFT = 0;
+            int GEAR_SHIFT = -1;
         }
 
         public interface Shooter {}
@@ -52,5 +56,65 @@ public final class Constants {
         public interface Climber {}
 
         public interface Intake {}
+    }
+
+    public interface DrivetrainSettings {
+        // If speed is below this, use quick turn
+        SmartNumber BASE_TURNING_SPEED = new SmartNumber("Driver Settings/Base Turn Speed", 0.4);
+
+        // Low Pass Filter and deadband for Driver Controls
+        SmartNumber SPEED_DEADBAND = new SmartNumber("Driver Settings/Speed Deadband", 0.0);
+        SmartNumber ANGLE_DEADBAND = new SmartNumber("Driver Settings/Turn Deadband", 0.0);
+
+        SmartNumber SPEED_POWER = new SmartNumber("Driver Settings/Speed Power", 1.0);
+        SmartNumber ANGLE_POWER = new SmartNumber("Driver Settings/Turn Power", 1.0);
+
+        SmartNumber SPEED_FILTER = new SmartNumber("Driver Settings/Speed Filtering", 0.2);
+        SmartNumber ANGLE_FILTER = new SmartNumber("Driver Settings/Turn Filtering", 0.01);
+
+        // Current Limit for the motors
+        int CURRENT_LIMIT = 30; // TODO: figure out SMART current limit
+
+        // If the motors are inverted
+        boolean IS_INVERTED = true;
+
+        // Width of the robot
+        double TRACK_WIDTH = Units.inchesToMeters(-1); // TODO: do it
+
+        interface Motion {
+
+            DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(TRACK_WIDTH);
+
+            SimpleMotorFeedforward MOTOR_FEED_FORWARD =
+                    new SimpleMotorFeedforward(FeedForward.S, FeedForward.V, FeedForward.A);
+
+            interface FeedForward {
+                double S = -1; // TODO: characterize
+                double V = -1; // TODO: characterize
+                double A = -1; // TODO: characterize
+            }
+
+            interface PID {
+                double P = -1; // TODO: characterize
+                double I = -1; // TODO: characterize
+                double D = -1; // TODO: characterize
+            }
+        }
+
+        public interface Odometry {
+            Translation2d STARTING_TRANSLATION = new Translation2d();
+            Rotation2d STARTING_ANGLE = new Rotation2d();
+
+            Pose2d STARTING_POSITION = new Pose2d(STARTING_TRANSLATION, STARTING_ANGLE);
+        }
+
+        // Encoder Constants
+        public interface Encoders {
+            double WHEEL_DIAMETER = Units.inchesToMeters(4);
+            double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+
+            double LOW_GEAR_DISTANCE_PER_ROTATION = WHEEL_CIRCUMFERENCE * (1.0 / 16.71);
+            double HIGH_GEAR_DISTANCE_PER_ROTATION = WHEEL_CIRCUMFERENCE * (1.0 / 4.55);
+        }
     }
 }
