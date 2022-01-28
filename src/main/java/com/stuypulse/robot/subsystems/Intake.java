e901efc436dbb82ba792781c072d04155283dd50
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-01-27 21:21:16 -0500

    Port over Edwin intake and constants

--- src/main/java/com/stuypulse/robot/subsystems/Intake.java
+++ src/main/java/com/stuypulse/robot/subsystems/Intake.java
@@ -5,13 +5,25 @@
 
 package com.stuypulse.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.stuypulse.robot.Constants;
+import com.stuypulse.robot.Constants.IntakeSettings;
+import com.stuypulse.robot.Constants.Ports;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /*-
  * Intake balls into the robot
  *
  * Contains:
- *      - TBD
+ *      - Double Solenoid to lift and lower intake
+ *      - Motor to control rollers that intake the balls
  *
  * @author Elaine Ye
  * @author Ben Goldfisher
@@ -28,12 +40,55 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class Intake extends SubsystemBase {
 
+    private final CANSparkMax motor;
+    private final DoubleSolenoid solenoid;
+    private DigitalInput sensor;
+
     public Intake() {
-        // Initialize Motors, etc... here!
+        motor = new CANSparkMax(Ports.Intake.MOTOR, MotorType.kBrushless);
+        // TODO: ask engineering if this is the right control system
+        // https://docs.wpilib.org/en/stable/docs/software/hardware-apis/pneumatics/pneumatics.html
+        solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Ports.Intake.SOLENOID_A, Ports.Intake.SOLENOID_B);
+        sensor = new DigitalInput(Ports.Intake.SENSOR);
+
+        addChild("Double Solenoid", solenoid);
+        addChild("Sensor", sensor);
+    }
+
+    /*** Extend / Retract ***/
+    public void extend() {
+        solenoid.set(Value.kReverse);
+    }
+
+    public void retract() {
+        solenoid.set(Value.kForward);
+    }
+
+    /*** Acquire / Deaqcuire ***/
+    public void setMotor(final double speed) {
+        motor.set(speed);
+    }
+
+    public void stop() {
+        motor.stopMotor();
+    }
+
+    public void acquire() {
+        setMotor(IntakeSettings.MOTOR_SPEED);
+    }
+
+    public void deacquire() {
+        setMotor(-IntakeSettings.MOTOR_SPEED);
+    }
+
+    public boolean isBallDetected() {
+        return !sensor.get();
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        if (Constants.DEBUG_MODE.get()) {
+            SmartDashboard.putBoolean("Intake/Ball Detected", isBallDetected());
+        }
     }
 }
