0e984bb7ba0b6a8462b84c239dead00d6f8a2824
Myles Pasetsky <34213819+selym3@users.noreply.github.com>
2022-01-28 18:16:47 -0500

    Complete Dorcas Drivetrain (#4)
    * Port over Edwin drivetrain and constants
    
    * Add drivetrain CanIDs
    
    * made good stuff

--- src/main/java/com/stuypulse/robot/Constants.java
+++ src/main/java/com/stuypulse/robot/Constants.java
@@ -8,6 +8,12 @@ package com.stuypulse.robot;
 import com.stuypulse.stuylib.network.SmartBoolean;
 import com.stuypulse.stuylib.network.SmartNumber;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
@@ -36,18 +42,15 @@ public interface Constants {
         }
 
         public interface Drivetrain {
-            int LEFT_TOP = 7;
-            int LEFT_BOTTOM = 6;
+            int LEFT_TOP = 10;
+            int LEFT_MIDDLE = 11;
+            int LEFT_BOTTOM = 12;
 
-            int RIGHT_TOP = 4;
-            int RIGHT_BOTTOM = 3;
+            int RIGHT_TOP = 13;
+            int RIGHT_MIDDLE = 14;
+            int RIGHT_BOTTOM = 15;
 
-            int LEFT_ENCODER_A = 0;
-            int LEFT_ENCODER_B = 1;
-            int RIGHT_ENCODER_A = 2;
-            int RIGHT_ENCODER_B = 3;
-
-            int GEAR_SHIFT = 0;
+            int GEAR_SHIFT = -1;
         }
 
         public interface Shooter {
@@ -101,6 +104,66 @@ public interface Constants {
             double kI = 0.0;
             double kD = 0.0;
             double kF = 0.0;
+        }
+    }
+
+    public interface DrivetrainSettings {
+        // If speed is below this, use quick turn
+        SmartNumber BASE_TURNING_SPEED = new SmartNumber("Driver Settings/Base Turn Speed", 0.4);
+
+        // Low Pass Filter and deadband for Driver Controls
+        SmartNumber SPEED_DEADBAND = new SmartNumber("Driver Settings/Speed Deadband", 0.0);
+        SmartNumber ANGLE_DEADBAND = new SmartNumber("Driver Settings/Turn Deadband", 0.0);
+
+        SmartNumber SPEED_POWER = new SmartNumber("Driver Settings/Speed Power", 1.0);
+        SmartNumber ANGLE_POWER = new SmartNumber("Driver Settings/Turn Power", 1.0);
+
+        SmartNumber SPEED_FILTER = new SmartNumber("Driver Settings/Speed Filtering", 0.2);
+        SmartNumber ANGLE_FILTER = new SmartNumber("Driver Settings/Turn Filtering", 0.01);
+
+        // Current Limit for the motors
+        int CURRENT_LIMIT = 30; // TODO: figure out SMART current limit
+
+        // If the motors are inverted
+        boolean IS_INVERTED = true;
+
+        // Width of the robot
+        double TRACK_WIDTH = Units.inchesToMeters(26.9); // SEAN PROMISED !
+
+        interface Motion {
+
+            DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(TRACK_WIDTH);
+
+            SimpleMotorFeedforward MOTOR_FEED_FORWARD =
+                    new SimpleMotorFeedforward(FeedForward.S, FeedForward.V, FeedForward.A);
+
+            interface FeedForward {
+                double S = -1; // TODO: characterize
+                double V = -1; // TODO: characterize
+                double A = -1; // TODO: characterize
+            }
+
+            interface PID {
+                double P = -1; // TODO: characterize
+                double I = -1; // TODO: characterize
+                double D = -1; // TODO: characterize
+            }
+        }
+
+        public interface Odometry {
+            Translation2d STARTING_TRANSLATION = new Translation2d();
+            Rotation2d STARTING_ANGLE = new Rotation2d();
+
+            Pose2d STARTING_POSITION = new Pose2d(STARTING_TRANSLATION, STARTING_ANGLE);
+        }
+
+        // Encoder Constants
+        public interface Encoders {
+            double WHEEL_DIAMETER = Units.inchesToMeters(4);
+            double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+
+            double LOW_GEAR_DISTANCE_PER_ROTATION = WHEEL_CIRCUMFERENCE * (1.0 / 16.71);
+            double HIGH_GEAR_DISTANCE_PER_ROTATION = WHEEL_CIRCUMFERENCE * (1.0 / 4.55);
         }
     }
 }
--- src/main/java/com/stuypulse/robot/subsystems/Drivetrain.java
+++ src/main/java/com/stuypulse/robot/subsystems/Drivetrain.java
@@ -5,6 +5,32 @@
 
 package com.stuypulse.robot.subsystems;
 
+import com.stuypulse.stuylib.math.Angle;
+import com.stuypulse.stuylib.math.SLMath;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
+import com.stuypulse.robot.Constants;
+import com.stuypulse.robot.Constants.DrivetrainSettings;
+import com.stuypulse.robot.Constants.Ports;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /*-
@@ -21,15 +47,398 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  *
  * @author Myles Pasetsky
  * @author Sam Belliveau
+ * @author Samuel Chen
+ * @author Andrew Liu
+ * @author Ryan Zhou
+ * @author Jack Zheng
+ * @author Daniel Chang
+ * @author Benjamin Beloster
+ * @author Russell Goyachev
+ * @author James Li
+ * @author Zhi Ming Xu
  */
 public class Drivetrain extends SubsystemBase {
 
+    // Enum used to store the state of the gear
+    public static enum Gear {
+        HIGH,
+        LOW
+    };
+
+    // An array of motors on the left and right side of the drive train
+    private CANSparkMax[] leftMotors;
+    private CANSparkMax[] rightMotors;
+
+    // An encoder for each side of the drive train
+    private RelativeEncoder leftNEO;
+    private RelativeEncoder rightNEO;
+
+    // DifferentialDrive and Gear Information
+    private Gear gear;
+    private Solenoid gearShift;
+    private DifferentialDrive drivetrain;
+
+    // NAVX for Gyro
+    private AHRS navx;
+
+    // Odometry
+    private DifferentialDriveOdometry odometry;
+    private Field2d field;
+
     public Drivetrain() {
-        // Initialize Motors, etc... here!
+        // Add Motors to list
+        leftMotors =
+                new CANSparkMax[] {
+                    new CANSparkMax(Ports.Drivetrain.LEFT_TOP, MotorType.kBrushless),
+                    new CANSparkMax(Ports.Drivetrain.LEFT_MIDDLE, MotorType.kBrushless),
+                    new CANSparkMax(Ports.Drivetrain.LEFT_BOTTOM, MotorType.kBrushless)
+                };
+
+        rightMotors =
+                new CANSparkMax[] {
+                    new CANSparkMax(Ports.Drivetrain.RIGHT_TOP, MotorType.kBrushless),
+                    new CANSparkMax(Ports.Drivetrain.RIGHT_MIDDLE, MotorType.kBrushless),
+                    new CANSparkMax(Ports.Drivetrain.RIGHT_BOTTOM, MotorType.kBrushless)
+                };
+
+        // Create list of encoders based on motors
+        leftNEO = leftMotors[0].getEncoder();
+        rightNEO = rightMotors[0].getEncoder();
+
+        leftNEO.setPosition(0);
+        rightNEO.setPosition(0);
+
+        // Make differential drive object
+        drivetrain =
+                new DifferentialDrive(
+                        new MotorControllerGroup(leftMotors),
+                        new MotorControllerGroup(rightMotors));
+
+        // Add Gear Shifter
+        gearShift = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Drivetrain.GEAR_SHIFT);
+
+        // Initialize NAVX
+        navx = new AHRS(SPI.Port.kMXP);
+
+        // Initialize Odometry
+        odometry =
+                new DifferentialDriveOdometry(
+                        DrivetrainSettings.Odometry.STARTING_ANGLE,
+                        DrivetrainSettings.Odometry.STARTING_POSITION);
+        field = new Field2d();
+
+        // Configure Motors and Other Things
+        setInverted(DrivetrainSettings.IS_INVERTED, !DrivetrainSettings.IS_INVERTED);
+        setSmartCurrentLimit(DrivetrainSettings.CURRENT_LIMIT);
+        setIdleMode(IdleMode.kBrake);
+        setHighGear();
+
+        // Add Children to Subsystem
+        addChild("Gear Shift", gearShift);
+        addChild("Differential Drive", drivetrain);
+        addChild("NavX", navx);
+        addChild("Field Map", field);
     }
+
+    /***********************
+     * MOTOR CONFIGURATION *
+     ***********************/
+
+    // Set the distance traveled in one rotation of the motor
+    public void setNEODistancePerRotation(double distance) {
+        leftNEO.setPositionConversionFactor(distance);
+        rightNEO.setPositionConversionFactor(distance);
+    }
+
+    // Set the smart current limit of all the motors
+    public void setSmartCurrentLimit(int limit) {
+        for (CANSparkMax motor : leftMotors) {
+            motor.setSmartCurrentLimit(limit);
+        }
+
+        for (CANSparkMax motor : rightMotors) {
+            motor.setSmartCurrentLimit(limit);
+        }
+    }
+
+    // Set the idle mode of the all the motors
+    public void setIdleMode(IdleMode mode) {
+        for (CANSparkMax motor : leftMotors) {
+            motor.setIdleMode(mode);
+        }
+
+        for (CANSparkMax motor : rightMotors) {
+            motor.setIdleMode(mode);
+        }
+    }
+
+    // Set isInverted of all the motors
+    public void setInverted(boolean leftSide, boolean rightSide) {
+        leftNEO.setInverted(leftSide);
+        for (CANSparkMax motor : leftMotors) {
+            motor.setInverted(leftSide);
+        }
+
+        rightNEO.setInverted(rightSide);
+        for (CANSparkMax motor : rightMotors) {
+            motor.setInverted(rightSide);
+        }
+    }
+
+    /*****************
+     * Gear Shifting *
+     *****************/
+
+    // Gets the current gear the robot is in
+    public Gear getGear() {
+        return gear;
+    }
+
+    // Sets the current gear the robot is in
+    public void setGear(Gear gear) {
+        if (this.gear != gear) {
+            this.gear = gear;
+            if (this.gear == Gear.HIGH) {
+                gearShift.set(true);
+                setNEODistancePerRotation(
+                        DrivetrainSettings.Encoders.HIGH_GEAR_DISTANCE_PER_ROTATION);
+                reset();
+            } else {
+                gearShift.set(false);
+                setNEODistancePerRotation(
+                        DrivetrainSettings.Encoders.LOW_GEAR_DISTANCE_PER_ROTATION);
+                reset();
+            }
+        }
+    }
+
+    // Sets robot into low gear
+    public void setLowGear() {
+        setGear(Gear.LOW);
+    }
+
+    // Sets robot into high gear
+    public void setHighGear() {
+        setGear(Gear.HIGH);
+    }
+
+    /********
+     * NAVX *
+     ********/
+
+    // Gets current Angle of the Robot as a double (contiuous / not +-180)
+    public double getRawAngle() {
+        return navx.getAngle();
+    }
+
+    // Gets current Angle of the Robot
+    public Angle getAngle() {
+        return Angle.fromDegrees(getRawAngle());
+    }
+
+    private void resetNavX() {
+        navx.reset();
+    }
+
+    /*********************
+     * ENCODER FUNCTIONS *
+     *********************/
+
+    // Distance
+    public double getLeftDistance() {
+        return leftNEO.getPosition();
+    }
+
+    public double getRightDistance() {
+        return rightNEO.getPosition();
+    }
+
+    public double getDistance() {
+        return (getLeftDistance() + getRightDistance()) / 2.0;
+    }
+
+    // Velocity
+    public double getLeftVelocity() {
+        return leftNEO.getVelocity();
+    }
+
+    public double getRightVelocity() {
+        return rightNEO.getVelocity();
+    }
+
+    public double getVelocity() {
+        return (getLeftVelocity() + getRightVelocity()) / 2.0;
+    }
+
+    // Angle
+    private double getEncoderRotations() {
+        // this distance is in meters (converted from rotations by the encoder)
+        double distance = getLeftDistance() - getRightDistance();
+
+        // undo the conversion done by the encoder to get rotations
+        return distance / DrivetrainSettings.TRACK_WIDTH;
+    }
+
+    public Angle getEncoderAngle() {
+        return Angle.fromRotations(getEncoderRotations());
+    }
+
+    /**********************
+     * ODOMETRY FUNCTIONS *
+     **********************/
+
+    private void updateOdometry() {
+        odometry.update(getRotation2d(), getLeftDistance(), getRightDistance());
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(getLeftVelocity(), getRightVelocity());
+    }
+
+    public Rotation2d getRotation2d() {
+        // TODO: check if this needs to be negative
+        return getAngle().negative().getRotation2d();
+    }
+
+    public Pose2d getPose() {
+        updateOdometry();
+        return odometry.getPoseMeters();
+    }
+
+    public Field2d getField() {
+        return field;
+    }
+
+    /************************
+     * OVERALL SENSOR RESET *
+     ************************/
+
+    public void reset(Pose2d location) {
+        resetNavX();
+        leftNEO.setPosition(0);
+        rightNEO.setPosition(0);
+        odometry.resetPosition(location, getAngle().getRotation2d());
+    }
+
+    public void reset() {
+        reset(getPose());
+    }
+
+    /*********************
+     * VOLTAGE FUNCTIONS *
+     *********************/
+
+    public double getBatteryVoltage() {
+        return RobotController.getBatteryVoltage();
+    }
+
+    public double getLeftVoltage() {
+        return leftMotors[0].get() * getBatteryVoltage();
+    }
+
+    public double getRightVoltage() {
+        return rightMotors[0].get() * getBatteryVoltage();
+    }
+
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        for (MotorController motor : leftMotors) {
+            motor.setVoltage(leftVolts);
+        }
+
+        for (MotorController motor : rightMotors) {
+            motor.setVoltage(rightVolts);
+        }
+
+        drivetrain.feed();
+    }
+
+    /********************
+     * DRIVING COMMANDS *
+     ********************/
+
+    // Stops drivetrain from moving
+    public void stop() {
+        drivetrain.stopMotor();
+    }
+
+    // Drives using tank drive
+    public void tankDrive(double left, double right) {
+        drivetrain.tankDrive(left, right, false);
+    }
+
+    // Drives using arcade drive
+    public void arcadeDrive(double speed, double rotation) {
+        drivetrain.arcadeDrive(speed, rotation, false);
+    }
+
+    // Drives using curvature drive algorithm
+    public void curvatureDrive(double xSpeed, double zRotation, double baseTS) {
+        // Clamp all inputs to valid values;
+        xSpeed = SLMath.clamp(xSpeed, -1.0, 1.0);
+        zRotation = SLMath.clamp(zRotation, -1.0, 1.0);
+        baseTS = SLMath.clamp(baseTS, 0.0, 1.0);
+
+        // Find the amount to slow down turning by.
+        // This is proportional to the speed but has a base value
+        // that it starts from (allows turning in place)
+        double turnAdj = Math.max(baseTS, Math.abs(xSpeed));
+
+        // Find the speeds of the left and right wheels
+        double lSpeed = xSpeed + zRotation * turnAdj;
+        double rSpeed = xSpeed - zRotation * turnAdj;
+
+        // Find the maximum output of the wheels, so that if a wheel tries to go > 1.0
+        // it will be scaled down proportionally with the other wheels.
+        double scale = Math.max(1.0, Math.max(Math.abs(lSpeed), Math.abs(rSpeed)));
+
+        lSpeed /= scale;
+        rSpeed /= scale;
+
+        // Feed the inputs to the drivetrain
+        drivetrain.tankDrive(lSpeed, rSpeed, false);
+    }
+
+    // Drives using curvature drive algorithm with automatic quick turn
+    public void curvatureDrive(double xSpeed, double zRotation) {
+        this.curvatureDrive(xSpeed, zRotation, DrivetrainSettings.BASE_TURNING_SPEED.get());
+    }
+
+    /*********************
+     * DEBUG INFORMATION *
+     *********************/
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        updateOdometry();
+        field.setRobotPose(getPose());
+
+        // Smart Dashboard Information
+
+        if (Constants.DEBUG_MODE.get()) {
+
+            SmartDashboard.putData("Drivetrain/Field", field);
+            SmartDashboard.putString(
+                    "Drivetrain/Current Gear",
+                    getGear().equals(Gear.HIGH) ? "High Gear" : "Low Gear");
+            SmartDashboard.putNumber("Drivetrain/Odometer X Position (m)", getPose().getX());
+            SmartDashboard.putNumber("Drivetrain/Odometer Y Position (m)", getPose().getY());
+            SmartDashboard.putNumber(
+                    "Drivetrain/Odometer Rotation (deg)", getPose().getRotation().getDegrees());
+
+            SmartDashboard.putNumber("Drivetrain/Motor Voltage Left (V)", getLeftVoltage());
+            SmartDashboard.putNumber("Drivetrain/Motor Voltage Right (V)", getRightVoltage());
+
+            SmartDashboard.putNumber("Drivetrain/Distance Traveled (m)", getDistance());
+            SmartDashboard.putNumber("Drivetrain/Distance Traveled Left (m)", getLeftDistance());
+            SmartDashboard.putNumber("Drivetrain/Distance Traveled Right (m)", getRightDistance());
+
+            SmartDashboard.putNumber("Drivetrain/Velocity (m per s)", getVelocity());
+            SmartDashboard.putNumber("Drivetrain/Velocity Left (m per s)", getLeftVelocity());
+            SmartDashboard.putNumber("Drivetrain/Velocity Right (m per s)", getRightVelocity());
+
+            SmartDashboard.putNumber("Drivetrain/Angle NavX (deg)", getAngle().toDegrees());
+            SmartDashboard.putNumber(
+                    "Drivetrain/Encoder Angle (deg)", getEncoderAngle().toDegrees());
+        }
     }
 }
