c4637a9dff175ec95a2860921b75d8e65add9750
Andrew <an6reww@gmail.com>
2022-02-02 19:10:19 -0500

    Add drivetrain high gear command

---
+++ src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainHighGearCommand.java
@@ -0,0 +1,16 @@
+package com.stuypulse.robot.commands.drivetrain;
+import com.stuypulse.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+public class DrivetrainHighGearCommand extends InstantCommand{
+    private Drivetrain drivetrain;
+
+    public DrivetrainHighGearCommand(Drivetrain drivetrain){
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void initialize(){
+        drivetrain.setHighGear();
+    }
+}
