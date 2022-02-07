1c6d7b263f3a0f12df86d3a0aae30eb005db3cde
an6reww <an6reww@gmail.com>
2022-02-07 17:28:27 -0500

    Add Mobility Auton Command With Encoders

--- src/main/java/com/stuypulse/robot/commands/auton/MobilityAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/MobilityAuton.java
@@ -1,13 +1,41 @@
 package com.stuypulse.robot.commands.auton;
 
+import com.stuypulse.robot.Constants;
 import com.stuypulse.robot.RobotContainer;
+import com.stuypulse.robot.commands.drivetrain.DrivetrainDriveDistanceCommand;
 import com.stuypulse.robot.commands.drivetrain.DrivetrainDriveForeverCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import com.stuypulse.robot.commands.leds.LEDSetCommand;
+import com.stuypulse.robot.subsystems.LEDController.LEDColor;
 
-public class MobilityAuton extends SequentialCommandGroup{
-    public MobilityAuton(RobotContainer robot){
-        addCommands(
-            new DrivetrainDriveForeverCommand(robot.drivetrain, -1.0).withTimeout(5)
-        );
-    }    
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class MobilityAuton {
+    public static class NoEncoders extends SequentialCommandGroup {
+        public NoEncoders(RobotContainer robot){
+            addCommands(
+                new DrivetrainDriveForeverCommand(robot.drivetrain, 1.0).withTimeout(5)
+            );
+        }
+    }
+    public static class WithEncoders extends SequentialCommandGroup{
+        public WithEncoders(RobotContainer robot){
+            addCommands(
+                new DrivetrainDriveDistanceCommand
+                (robot.drivetrain, Constants.DrivetrainSettings.TRACK_WIDTH * 3),
+
+                new LEDSetCommand(robot.leds, LEDColor.BEAT),
+                new WaitCommand(0.5),
+                new LEDSetCommand(robot.leds, LEDColor.CONFETTI),
+                new WaitCommand(0.5),
+                new LEDSetCommand(robot.leds, LEDColor.RAINBOW),
+                new WaitCommand(0.5),
+                new LEDSetCommand(robot.leds, LEDColor.LIME_SOLID),
+                new WaitCommand(0.5),
+                new LEDSetCommand(robot.leds, LEDColor.ORANGE_PULSE),
+                new WaitCommand(0.5),
+                new LEDSetCommand(robot.leds, LEDColor.RED_PULSE)
+            );
+        }
+    }
 }
