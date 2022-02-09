4f5f09b7a0c63034e836b53217d7b1fa7bdfe5e6
Shaurya Sen <shauryasen12@gmail.com>
2022-02-09 17:15:52 -0500

    remove k

--- src/main/java/com/stuypulse/robot/Constants.java
+++ src/main/java/com/stuypulse/robot/Constants.java
@@ -25,7 +25,7 @@ import java.nio.file.Path;
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static)k. Do not put anything functional in this class.
+ * globally (i.e. public static). Do not put anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
