52bb8b8efa0226c725c8b9c1dbe013c3a052d91e
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-02-21 16:42:37 -0500

    Document and add alternate version to align below a certain distance

--- src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainHybridRamseteAlignCommand.java
+++ src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainHybridRamseteAlignCommand.java
@@ -6,7 +6,9 @@
 package com.stuypulse.robot.commands.drivetrain;
 
 import com.stuypulse.robot.subsystems.Drivetrain;
+import com.stuypulse.robot.util.IFuser;
 import com.stuypulse.robot.util.Target;
+import com.stuypulse.robot.constants.Settings.Alignment;
 
 import edu.wpi.first.math.trajectory.Trajectory;
 
@@ -15,14 +17,43 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 /**
  * @author Ivan Wei (ivanw8288@gmail.com)
  * 
- * A command group that allows direct switch from path following into target alignment as soon as a target is acquired,
+ * A command group that allows direct switch from path following into target alignment
  * to avoid the extra period of time of alignment after completing a path.
  */
 public class DrivetrainHybridRamseteAlignCommand extends SequentialCommandGroup {
 
+    /**
+     * Creates a Hybrid Ramsete Align command which immediately swaps from path following
+     * to alignment as soon as a target is acquired.
+     * @param drivetrain drivetrain required for both ramsete and align commands
+     * @param trajectory see ramsete command
+     * @param targetDistance see align command
+     * @param alignmentTimeout time allowed for alignment
+     */
     public DrivetrainHybridRamseteAlignCommand(Drivetrain drivetrain, Trajectory trajectory, Number targetDistance, double alignmentTimeout) {
         addCommands(
             new DrivetrainRamseteCommand(drivetrain, trajectory).withInterrupt(() -> Target.hasTarget()),
+            new DrivetrainAlignCommand(drivetrain, targetDistance).withTimeout(alignmentTimeout)
+        );
+    }
+
+    /**
+     * Creates a Hybrid Ramsete Align command which swaps from path following to alignment when
+     * a target is acquired AND the robot is below a certain distance away.
+     * @param drivetrain drivetrain required for both ramsete and align commands
+     * @param trajectory see ramsete command
+     * @param targetDistance see align command
+     * @param alignmentTimeout time allowed for alignment
+     * @param swapDistance distance error at which below to switch to alignment
+     */
+    public DrivetrainHybridRamseteAlignCommand(Drivetrain drivetrain, Trajectory trajectory, Number targetDistance, double alignmentTimeout, double swapDistance) {
+        IFuser distanceError =
+            new IFuser(
+                Alignment.FUSION_FILTER,
+                () -> Target.getDistance() - targetDistance.doubleValue(),
+                () -> drivetrain.getDistance());
+        addCommands(
+            new DrivetrainRamseteCommand(drivetrain, trajectory).withInterrupt(() -> Target.hasTarget() && (distanceError.get() < swapDistance)),
             new DrivetrainAlignCommand(drivetrain, targetDistance).withTimeout(alignmentTimeout)
         );
     }
