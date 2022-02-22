fcdf8ad6b3b576007ff78f2475ca4d7de549e89e
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-02-22 17:29:55 -0500

    Try to create an alternate version

--- src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainHybridRamseteAlignCommand.java
+++ src/main/java/com/stuypulse/robot/commands/drivetrain/DrivetrainHybridRamseteAlignCommand.java
@@ -5,12 +5,11 @@
 
 package com.stuypulse.robot.commands.drivetrain;
 
+import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.subsystems.Drivetrain;
 import com.stuypulse.robot.util.Target;
 
-import edu.wpi.first.math.trajectory.Trajectory;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
  * @author Ivan Wei (ivanw8288@gmail.com)
@@ -18,42 +17,51 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
  * A command group that allows direct switch from path following into target alignment
  * to avoid the extra period of time of alignment after completing a path.
  */
-public class DrivetrainHybridRamseteAlignCommand extends SequentialCommandGroup {
-
+public class DrivetrainHybridRamseteAlignCommand extends CommandBase {
+    private static final double SWAP_DISTANCE = Settings.Limelight.RING_SHOT_DISTANCE + 2 * Settings.Limelight.INTAKE_TO_LIMELIGHT;
+    private final DrivetrainRamseteCommand ramseteCommand;
+    private final DrivetrainAlignCommand alignCommand;
+    private boolean switched;
     /**
      * Creates a Hybrid Ramsete Align command which immediately swaps from path following
      * to alignment as soon as a target is acquired.
      * @param alignmentTimeout time allowed for alignment
      */
-    public DrivetrainHybridRamseteAlignCommand(DrivetrainRamseteCommand ramseteCommand, DrivetrainAlignCommand alignCommand, double alignmentTimeout) {
-        addCommands(
-            ramseteCommand.withInterrupt(() -> Target.hasTarget()),
-            alignCommand.withTimeout(alignmentTimeout)
-        );
-    }
+    public DrivetrainHybridRamseteAlignCommand(Drivetrain drivetrain, DrivetrainRamseteCommand ramseteCommand, DrivetrainAlignCommand alignCommand, double alignmentTimeout) {
+        
+        this.ramseteCommand = ramseteCommand;
+        this.alignCommand = alignCommand;
 
-    /**
-     * Creates a Hybrid Ramsete Align command which swaps from path following to alignment when
-     * a target is acquired AND the robot is below a certain distance away.
-     * @param alignmentTimeout time allowed for alignment
-     * @param swapDistance distance error at which below to switch to alignment
-     */
-    public DrivetrainHybridRamseteAlignCommand(DrivetrainRamseteCommand ramseteCommand, DrivetrainAlignCommand alignCommand, double alignmentTimeout, double swapDistance) {
-        addCommands(
-            ramseteCommand.withInterrupt(() -> Target.hasTarget() && (Target.getDistance() < swapDistance)),
-            alignCommand.withTimeout(alignmentTimeout)
-        );
+        switched = false;
+
+        addRequirements(drivetrain);
     }
 
     @Override
     public void initialize() {
-        // Although AlignCommand will enable the limelight LED, we want it on before to check 
-        // if a target enters range to run AlignCommand in the first place
-        Target.enable();
+        ramseteCommand.initialize();
+    }
+
+    @Override
+    public void execute() {
+        
+        if (switched) {
+            alignCommand.execute();
+            return;
+        }
+
+        if(!switched && Target.getDistance() < SWAP_DISTANCE) {
+            switched = true;
+            alignCommand.initialize();
+            return;
+        }
+
+        ramseteCommand.execute();
     }
 
     @Override
     public void end(boolean interrupted) {
-        Target.disable();
+        alignCommand.end(false);
+        ramseteCommand.end(false);
     }
 }
