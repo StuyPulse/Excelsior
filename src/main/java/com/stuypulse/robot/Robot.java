a05fdc3f7d0e3c13933f225cf82316c204b3f5e1
definatelyVincentWang <vinowang921@gmail.com>
2022-02-25 14:58:28 -0500

    reset in teleop init

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -5,6 +5,10 @@
 
 package com.stuypulse.robot;
 
+import com.stuypulse.robot.commands.conveyor.modes.ConveyorMode;
+import com.stuypulse.robot.subsystems.Climber.Tilt;
+import com.stuypulse.robot.subsystems.Conveyor.Direction;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -16,9 +20,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-    private Command m_autonomousCommand;
+    private Command auto;
 
-    private RobotContainer m_robotContainer;
+    private RobotContainer robot;
 
     /**
      * This function is run when the robot is first started up and should be used for any
@@ -28,7 +32,7 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
-        m_robotContainer = new RobotContainer();
+        robot = new RobotContainer();
     }
 
     /**
@@ -59,13 +63,11 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
-        m_robotContainer.colorSensor.getUpdateFromDriverStation();
-
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        auto = robot.getAutonomousCommand();
 
         // schedule the autonomous command (example)
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.schedule();
+        if (auto != null) {
+            auto.schedule();
         }
     }
 
@@ -75,14 +77,23 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        m_robotContainer.colorSensor.getUpdateFromDriverStation();
+        robot.colorSensor.getUpdateFromDriverStation();
+        robot.intake.stop();
+
+        robot.climber.setTilt(Tilt.NO_TILT);
+
+        robot.conveyor.setMode(ConveyorMode.DEFAULT);
+        robot.conveyor.setTopBelt(Direction.STOPPED);
+        robot.conveyor.setGandalf(Direction.STOPPED);
+
+        
 
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.cancel();
+        if (auto != null) {
+            auto.cancel();
         }
     }
 
