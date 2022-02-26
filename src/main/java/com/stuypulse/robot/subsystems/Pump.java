98617ce5ef25d93b6d05392acd41a9b944f2b605
definatelyVincentWang <vinowang921@gmail.com>
2022-02-26 13:13:50 -0500

    Automatic Pump Disabling

--- src/main/java/com/stuypulse/robot/subsystems/Pump.java
+++ src/main/java/com/stuypulse/robot/subsystems/Pump.java
@@ -7,6 +7,8 @@ package com.stuypulse.robot.subsystems;
 
 import com.stuypulse.stuylib.network.SmartBoolean;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -26,9 +28,13 @@ public class Pump extends SubsystemBase {
     private final SmartBoolean enabled;
     private final Compressor compressor;
 
+    private final Debouncer disable;
+
     public Pump() {
         enabled = new SmartBoolean("Pump/Compressor Enabled", true);
         compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+
+        disable = new Debouncer(1.0, DebounceType.kRising);
 
         stop();
     }
@@ -52,12 +58,20 @@ public class Pump extends SubsystemBase {
         enabled.set(compressing);
     }
 
+    // Determine if we should disable the pump
+    private boolean shouldDisable() {
+        return disable.calculate(
+            enabled.get() &&
+            !getCompressing()
+        );
+    }
+
     @Override
     public void periodic() {
         if (enabled.get()) {
             compressor.enableDigital();
 
-            if(!getCompressing()) {
+            if (shouldDisable()) {
                 stop();
             }
         } else {
