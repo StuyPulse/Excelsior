a9f91a8668691f70dea9e4f80cff66a62184fbf5
an6reww <an6reww@gmail.com>
2022-02-26 13:59:56 -0500

    Add more LED Colors

--- src/main/java/com/stuypulse/robot/subsystems/LEDController.java
+++ src/main/java/com/stuypulse/robot/subsystems/LEDController.java
@@ -10,7 +10,6 @@ import com.stuypulse.stuylib.util.StopWatch;
 import com.stuypulse.robot.RobotContainer;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.robot.constants.Settings;
-import com.stuypulse.robot.subsystems.ColorSensor.CurrentBall;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
@@ -28,11 +27,15 @@ public class LEDController extends SubsystemBase {
 
     // Enum that represents and calculates values for
     public enum LEDColor {
-        RAINBOW(-0.97, false),
+        RAINBOW(-0.99, false),
         SINELON(-0.77, false),
         CONFETTI(-0.87, false),
+        FIRE(0.57,false),
+        TWINKLES(-0.53,false),
         BEAT(-0.67, false),
         WAVE(-0.43, false),
+        HEARTBEAT(-0.25,false),
+        BREATH(-0.17,false),
 
         WHITE_SOLID(0.93, false),
         PINK_SOLID(0.57, false),
@@ -43,6 +46,22 @@ public class LEDController extends SubsystemBase {
         BLUE_SOLID(0.87, false),
         PURPLE_SOLID(0.91, false),
         YELLOW_SOLID(0.69, false),
+        HOTPINK_SOLID(0.57, false),
+        DARKRED_SOLID(0.59, false),
+        REDORANGE_SOLID(0.63, false),
+        GOLD_SOLID(0.67, false),
+        LAWNGREEN_SOLID(0.71,false),
+        DARKGREEN_SOLID(0.75,false),
+        BLUEGREEN_SOLID(0.79,false),
+        AQUA_SOLID(0.81,false),
+        SKYBLUE_SOLID(0.83,false),
+        DARKBLUE_SOLID(0.85,false),
+        BLUEVIOLET_SOLID(0.89,false),
+        VIOLET_SOLID(0.91,false),
+        GRAY_SOLID(0.95,false),
+        DARKGRAY_SOLID(0.97,false),
+        BLACK_SOLID(0.99,false),
+
 
         WHITE_PULSE(0.93, true),
         PINK_PULSE(0.57, true),
@@ -53,6 +72,26 @@ public class LEDController extends SubsystemBase {
         BLUE_PULSE(0.87, true),
         PURPLE_PULSE(0.91, true),
         YELLOW_PULSE(0.69, true),
+        HOTPINK_PULSE(0.57, true),
+        DARKRED_PULSE(0.59, true),
+        REDORANGE_PULSE(0.63, true),
+        GOLD_PULSE(0.67, true),
+        LAWNGREEN_PULSE(0.71,true),
+        DARKGREEN_PULSE(0.75,true),
+        BLUEGREEN_PULSE(0.79,true),
+        AQUA_PULSE(0.81,true),
+        SKYBLUE_PULSE(0.83,true),
+        DARKBLUE_PULSE(0.85,true),
+        BLUEVIOLET_PULSE(0.89,true),
+        VIOLET_PULSE(0.91,true),
+        GRAY_PULSE(0.95,true),
+        DARKGRAY_PULSE(0.97,true),
+        BLACK_PULSE(0.99,true),
+
+        
+
+
+
 
         OFF(0.99, false);
 
@@ -85,7 +124,7 @@ public class LEDController extends SubsystemBase {
     private final StopWatch lastUpdate;
 
     // The robot container to get information from
-    private final RobotContainer robot;
+    private final RobotContainer robotContainer;
 
     // The current color to set the LEDs to
     private LEDColor manualColor;
@@ -93,7 +132,7 @@ public class LEDController extends SubsystemBase {
     public LEDController(RobotContainer container) {
         this.controller = new PWMSparkMax(Ports.LEDController.PWM_PORT);
         this.lastUpdate = new StopWatch();
-        this.robot = container;
+        this.robotContainer = container;
 
         setColor(LEDColor.OFF);
     }
@@ -104,33 +143,7 @@ public class LEDController extends SubsystemBase {
     }
 
     public LEDColor getDefaultColor() {
-        /**
-         * - In fender shot mode led white - Ring shot red
-         *
-         * <p>All other behavior overides it - When shooter is getting to speed not yet the rpm
-         * flashing the color its suppose to be - When robot aligning solid yellow - When alignment
-         * finished green .75 second - Pick up a ball and sensed in the color sensor flash color of
-         * ball .75 second blue/orange - Two correct ball green
-         */
-        if (robot.pump.getCompressing()) return LEDColor.PINK_PULSE;
-
-        if (robot.conveyor.isFull()) return LEDColor.GREEN_SOLID;
-
-        if (robot.colorSensor.hasBall()) {
-            if (robot.colorSensor.getCurrentBall() == CurrentBall.BLUE_BALL) {
-                return LEDColor.BLUE_SOLID;
-            } else {
-                return LEDColor.ORANGE_SOLID;
-            }
-        }
-
-        if (Math.abs(robot.shooter.getShooterRPM() - Settings.Shooter.RING_RPM.get()) < 200) {
-            return LEDColor.RED_PULSE;
-        }
-        if (Math.abs(robot.shooter.getShooterRPM() - Settings.Shooter.FENDER_RPM.get()) < 200) {
-            return LEDColor.WHITE_PULSE;
-        }
-
+        // TODO: Add if statements that check sensor values to update LEDController
         return LEDColor.OFF;
     }
 
