cc8f40ee3c4dfedabd761842bfd7afc6277eae32
an6reww <an6reww@gmail.com>
2022-02-26 14:14:14 -0500

    Add led colors

--- src/main/java/com/stuypulse/robot/subsystems/LEDController.java
+++ src/main/java/com/stuypulse/robot/subsystems/LEDController.java
@@ -23,16 +23,21 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  *      - getDefaultColor() : determines LED color if it is not set
  *
  * @author Sam Belliveau
+ * @author Andrew Liu
  */
 public class LEDController extends SubsystemBase {
 
     // Enum that represents and calculates values for
     public enum LEDColor {
-        RAINBOW(-0.97, false),
+        RAINBOW(-0.99, false),
         SINELON(-0.77, false),
         CONFETTI(-0.87, false),
+        FIRE(0.57,false),
+        TWINKLES(-0.53,false),
         BEAT(-0.67, false),
         WAVE(-0.43, false),
+        HEARTBEAT(-0.25,false),
+        BREATH(-0.17,false),
 
         WHITE_SOLID(0.93, false),
         PINK_SOLID(0.57, false),
@@ -43,6 +48,21 @@ public class LEDController extends SubsystemBase {
         BLUE_SOLID(0.87, false),
         PURPLE_SOLID(0.91, false),
         YELLOW_SOLID(0.69, false),
+        HOTPINK_SOLID(0.57, false),
+        DARKRED_SOLID(0.59, false),
+        REDORANGE_SOLID(0.63, false),
+        GOLD_SOLID(0.67, false),
+        LAWNGREEN_SOLID(0.71,false),
+        DARKGREEN_SOLID(0.75,false),
+        BLUEGREEN_SOLID(0.79,false),
+        AQUA_SOLID(0.81,false),
+        SKYBLUE_SOLID(0.83,false),
+        DARKBLUE_SOLID(0.85,false),
+        BLUEVIOLET_SOLID(0.89,false),
+        VIOLET_SOLID(0.91,false),
+        GRAY_SOLID(0.95,false),
+        DARKGRAY_SOLID(0.97,false),
+        BLACK_SOLID(0.99,false),
 
         WHITE_PULSE(0.93, true),
         PINK_PULSE(0.57, true),
@@ -53,6 +73,21 @@ public class LEDController extends SubsystemBase {
         BLUE_PULSE(0.87, true),
         PURPLE_PULSE(0.91, true),
         YELLOW_PULSE(0.69, true),
+        HOTPINK_PULSE(0.57, true),
+        DARKRED_PULSE(0.59, true),
+        REDORANGE_PULSE(0.63, true),
+        GOLD_PULSE(0.67, true),
+        LAWNGREEN_PULSE(0.71,true),
+        DARKGREEN_PULSE(0.75,true),
+        BLUEGREEN_PULSE(0.79,true),
+        AQUA_PULSE(0.81,true),
+        SKYBLUE_PULSE(0.83,true),
+        DARKBLUE_PULSE(0.85,true),
+        BLUEVIOLET_PULSE(0.89,true),
+        VIOLET_PULSE(0.91,true),
+        GRAY_PULSE(0.95,true),
+        DARKGRAY_PULSE(0.97,true),
+        BLACK_PULSE(0.99,true),
 
         OFF(0.99, false);
 
