16b1f039eea4688e8e26aad77356a71ad911b982
Jason <independence106@gmail.com>
2022-03-04 16:24:02 -0500

    cleaning up code

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -53,7 +53,6 @@ public class Robot extends TimedRobot {
     /** This function is called once each time the robot enters Disabled mode. */
     @Override
     public void disabledInit() {    
-        // new ClimberBrakeCommand(robot.climber).schedule();
     }
 
     @Override
