04e24cb78ffddd47c0e590a512b5c3a28deabfa5
Myles Pasetsky <mp3blue99@gmail.com>
2022-03-14 22:53:13 -0400

    implement rest of shooter api in pid shooter

--- src/main/java/com/stuypulse/robot/subsystems/PIDShooter.java
+++ src/main/java/com/stuypulse/robot/subsystems/PIDShooter.java
@@ -4,9 +4,12 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.robot.constants.Settings.Shooter;
 import com.stuypulse.robot.util.PIDFlywheel;
 import com.stuypulse.stuylib.network.SmartNumber;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PIDShooter extends SubsystemBase {
@@ -16,6 +19,8 @@ public class PIDShooter extends SubsystemBase {
     private final PIDFlywheel shooter;
     private final PIDFlywheel feeder;
 
+    private final Solenoid hood;
+
     public PIDShooter() {
         targetRPM = new SmartNumber("Shooter/Target RPM", 0.0);
         
@@ -23,12 +28,35 @@ public class PIDShooter extends SubsystemBase {
         CANSparkMax shooterMotor = new CANSparkMax(Ports.Shooter.LEFT_SHOOTER, MotorType.kBrushless);
         CANSparkMax shooterFollower = new CANSparkMax(Ports.Shooter.RIGHT_SHOOTER, MotorType.kBrushless);
 
-        shooter = new PIDFlywheel("Debug/Shooter/Shooter Flywheel", shooterMotor, null)
-            .addFollower(shooterFollower, true);
+        shooter = new PIDFlywheel(shooterMotor, Shooter.SHOOTER_FEED_FORWARD, Shooter.ShooterPID.getController());
+        shooter.addFollower(shooterFollower, true);
 
         // Setup feeder flywheel
         CANSparkMax feederMotor = new CANSparkMax(Ports.Shooter.FEEDER, MotorType.kBrushless);
-        feeder = new PIDFlywheel("Debug/Shooter/Feeder Flywheel", feederMotor, null);
+        feeder = new PIDFlywheel(feederMotor, Shooter.FEEDER_FEED_FORWARD, Settings.Shooter.FeederPID.getController());
+    
+        // Create hood solenoid
+        hood = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Shooter.HOOD_SOLENOID);
+    }
+
+    public void setShooterRPM(Number speed) {
+        targetRPM.set(speed);
+    }
+
+    public void extendHood() {
+        hood.set(true);
+    }
+
+    public void retractHood() {
+        hood.set(false);
+    }
+
+    public double getShooterRPM() {
+        return Math.abs(shooter.getVelocity());
+    }
+
+    public double getFeederRPM() {
+        return Math.abs(feeder.getVelocity());
     }
 
     @Override
