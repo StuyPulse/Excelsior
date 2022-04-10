b3db1294719a521f2301c9b38883c8c5d6e7a64e
Prog694 <prog692@gmail.com>
2022-04-10 15:03:20 -0400

    make partner ball code mean

--- src/main/java/com/stuypulse/robot/commands/auton/PartnerBallAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/PartnerBallAuton.java
@@ -6,10 +6,13 @@
 package com.stuypulse.robot.commands.auton;
 
 import com.stuypulse.robot.RobotContainer;
+import com.stuypulse.robot.commands.conveyor.ConveyorForceEject;
 import com.stuypulse.robot.commands.conveyor.ConveyorShoot;
 import com.stuypulse.robot.commands.drivetrain.DrivetrainAlign;
 import com.stuypulse.robot.commands.drivetrain.DrivetrainRamsete;
 import com.stuypulse.robot.commands.intake.IntakeAcquireForever;
+import com.stuypulse.robot.commands.intake.IntakeDeacquire;
+import com.stuypulse.robot.commands.intake.IntakeDeacquireForever;
 import com.stuypulse.robot.commands.intake.IntakeExtend;
 import com.stuypulse.robot.commands.leds.LEDSet;
 import com.stuypulse.robot.commands.shooter.ShooterFenderShot;
@@ -35,8 +38,14 @@ public class PartnerBallAuton extends SequentialCommandGroup {
     private static final double DRIVETRAIN_ALIGN_TIME = 3.0;
     // Time to acquire partner auton ball
     private static final double ACQUIRE_PARTNER_BALL_TIME = 5.0;
+    // Time for our partner to move back
+    private static final double PARTNER_MOVE_BACK_TIME = 0.5;
+
+    private static final double INTAKE_DEACQUIRE_TIME = 3.0;
 
     private static final String PARTNER_BALL_TO_RING_BALL = "ThreeBallPartnerAuton/output/acquire_first_ball.wpilib.json";
+    private static final String PARTNER_BALL_GET_OPPONENT_BALL = "ThreeBallPartnerAuton/output/opponent_ball.wpilib.json";
+    private static final String PARTNER_BALL_EJECT = "ThreeBallPartnerAuton/output/move_to_hanger.wpilib.json";
 
     /** Creates a new FiveBallAuton. */
     public PartnerBallAuton(RobotContainer robot) {
@@ -54,7 +63,8 @@ public class PartnerBallAuton extends SequentialCommandGroup {
         addCommands(
             new LEDSet(robot.leds, LEDColor.GREEN),
             new WaitCommand(ACQUIRE_PARTNER_BALL_TIME).withInterrupt(robot.conveyor::isFull),
-            new ConveyorShoot(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER));
+            new ConveyorShoot(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER),
+            new WaitCommand(PARTNER_MOVE_BACK_TIME));
 
         addCommands(
             new ShooterRingShot(robot.shooter),
@@ -62,16 +72,28 @@ public class PartnerBallAuton extends SequentialCommandGroup {
                     .robotRelative(),
             new WaitCommand(SHOOTER_TO_RING_DELAY)
         );
+
         
         addCommands(
             new LEDSet(robot.leds, LEDColor.GREEN.pulse()),
             new DrivetrainAlign(robot.drivetrain, robot.camera)
-                    .withTimeout(DRIVETRAIN_ALIGN_TIME)
+                    .withTimeout(DRIVETRAIN_ALIGN_TIME),
+            new LEDSet(robot.leds, LEDColor.RAINBOW),
+            new ConveyorShoot(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER)
         );
         
         addCommands(
+            new LEDSet(robot.leds, LEDColor.RED),
+            new DrivetrainRamsete(robot.drivetrain, PARTNER_BALL_GET_OPPONENT_BALL)
+        );
+
+        addCommands(
             new LEDSet(robot.leds, LEDColor.RAINBOW),
-            new ConveyorShoot(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER)
+            new DrivetrainRamsete(robot.drivetrain, PARTNER_BALL_EJECT),
+
+            new IntakeDeacquire(robot.intake)
+                    .alongWith(new ConveyorForceEject(robot.conveyor))
+                    .withTimeout(INTAKE_DEACQUIRE_TIME)
         );
 
         addCommands(new LEDSet(robot.leds, LEDColor.WHITE.pulse()));
