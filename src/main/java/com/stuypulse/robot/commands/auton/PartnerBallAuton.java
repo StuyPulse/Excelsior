0f9647e140c8b269f09b1baf435706ef9b2c4333
Prog694 <prog692@gmail.com>
2022-04-20 13:46:38 -0400

    update timeouts in partner ball

--- src/main/java/com/stuypulse/robot/commands/auton/PartnerBallAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/PartnerBallAuton.java
@@ -29,19 +29,21 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
  
 public class PartnerBallAuton extends SequentialCommandGroup {
     // Time it takes for the shooter to reach the target speed
-    private static final double SHOOTER_INITIALIZE_DELAY = 0.3;
+    private static final double SHOOTER_INITIALIZE_DELAY = 1.0;
     // Time it takes for shooter to go from Fender to Ring shot speed
     private static final double SHOOTER_TO_RING_DELAY = 0.2;
     // Time it takes for the conveyor to give the shooter the ball
-    private static final double CONVEYOR_TO_SHOOTER = 3.0;
+    private static final double CONVEYOR_TO_SHOOTER = 1.0;
     // Time we want to give the drivetrain to align
     private static final double DRIVETRAIN_ALIGN_TIME = 3.0;
     // Time to acquire partner auton ball
     private static final double ACQUIRE_PARTNER_BALL_TIME = 5.0;
     // Time for our partner to move back
-    private static final double PARTNER_MOVE_BACK_TIME = 0.5;
+    private static final double PARTNER_MOVE_BACK_TIME = 1.0;
 
     private static final double INTAKE_DEACQUIRE_TIME = 3.0;
+
+    private static final double RING_SHOT_DELAY = 4.0;
 
     private static final String PARTNER_BALL_TO_RING_BALL = "ThreeBallPartnerAuton/output/acquire_first_ball.wpilib.json";
     private static final String PARTNER_BALL_GET_OPPONENT_BALL = "ThreeBallPartnerAuton/output/opponent_ball.wpilib.json";
@@ -59,13 +61,16 @@ public class PartnerBallAuton extends SequentialCommandGroup {
             new WaitCommand(SHOOTER_INITIALIZE_DELAY)
         );
 
-        // Tarmac to first ball
+        // (acquire ball from partner)
+        // shoot top ball
         addCommands(
             new LEDSet(robot.leds, LEDColor.GREEN),
             new WaitCommand(ACQUIRE_PARTNER_BALL_TIME).withInterrupt(robot.conveyor::isFull),
             new ConveyorShootTop(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER),
             new WaitCommand(PARTNER_MOVE_BACK_TIME));
 
+        // enter ring mode
+        // run path to ring shot
         addCommands(
             new ShooterRingShot(robot.shooter),
             new DrivetrainRamsete(robot.drivetrain, PARTNER_BALL_TO_RING_BALL)
@@ -74,12 +79,13 @@ public class PartnerBallAuton extends SequentialCommandGroup {
         );
 
         
+        // align 
         addCommands(
             new LEDSet(robot.leds, LEDColor.GREEN.pulse()),
             new DrivetrainAlign(robot.drivetrain, robot.camera)
                     .withTimeout(DRIVETRAIN_ALIGN_TIME),
             new LEDSet(robot.leds, LEDColor.RAINBOW),
-            new ConveyorShoot(robot.conveyor).withTimeout(CONVEYOR_TO_SHOOTER)
+            new ConveyorShoot(robot.conveyor).withTimeout(RING_SHOT_DELAY)
         );
         
         addCommands(
