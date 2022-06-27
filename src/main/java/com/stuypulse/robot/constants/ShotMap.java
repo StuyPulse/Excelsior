2a97b83e6d627159244641d9607c507f8dcc060b
Eric Lin <72947759+cire694@users.noreply.github.com>
2022-06-27 00:18:10 -0400

    Add more shot map interpolation points
    * Add Standard deviation
    
    * Add more Interpolation points
    
    * Update ShotMap.java
    
    myles is being a poo poo
    
    * Create Distances interface in ShotMap.java
    
    * Undo redundant unit conversion
    
    * Rename points to match naming convention
    
    * rename min distance
    
    Co-authored-by: Myles Pasetsky <mp3blue99@gmail.com>

--- src/main/java/com/stuypulse/robot/constants/ShotMap.java
+++ src/main/java/com/stuypulse/robot/constants/ShotMap.java
@@ -9,20 +9,37 @@ import com.stuypulse.stuylib.math.Vector2D;
 import com.stuypulse.stuylib.math.interpolation.Interpolator;
 import com.stuypulse.stuylib.math.interpolation.NearestInterpolator;
 
+import edu.wpi.first.math.util.Units;
+
 import com.stuypulse.robot.constants.Settings.Limelight;
+
+
 
 /** Contains interpolation tables for shooting and alignment */
 public interface ShotMap {
+        
+        double MIN_DISTANCE = 0;
 
-    // Converts a distance measurement to an RPM to shoot at
-    Interpolator DISTANCE_TO_RPM =
-            new NearestInterpolator(
-                    new Vector2D(Limelight.RING_DISTANCE.get(), Settings.Shooter.RING_RPM.get()),
-                    new Vector2D(Limelight.PAD_DISTANCE.get(), Settings.Shooter.PAD_RPM.get()));
+        public interface Distances{
+                double RING = Limelight.RING_DISTANCE.get();
+                double POINT_A = Units.inchesToMeters(167);
+                double POINT_B = Units.inchesToMeters(184);
+                double LAUNCHPAD = Limelight.PAD_DISTANCE.get();
+        }       
 
-    // Converts a distance measurement to an angle offset to align to
-    Interpolator DISTANCE_TO_YAW =
-            new NearestInterpolator(
-                    new Vector2D(Limelight.RING_DISTANCE.get(), Limelight.RING_YAW.get()),
-                    new Vector2D(Limelight.PAD_DISTANCE.get(), Limelight.PAD_YAW.get()));
+        // Converts a distance measurement to an RPM to shoot at
+        Interpolator DISTANCE_TO_RPM =
+                new NearestInterpolator(
+                        new Vector2D(Distances.RING, Settings.Shooter.RING_RPM.get()),
+                        new Vector2D(Distances.POINT_A, 3050),
+                        new Vector2D(Distances.POINT_B, 3200),    
+                        new Vector2D(Distances.LAUNCHPAD, Settings.Shooter.PAD_RPM.get()));
+
+        // Converts a distance measurement to an angle offset to align to
+        Interpolator DISTANCE_TO_YAW =
+                new NearestInterpolator(
+                        new Vector2D(Distances.RING, Limelight.RING_YAW.get()),
+                        new Vector2D(Distances.POINT_A, 6.25),
+                        new Vector2D(Distances.POINT_B, 5.70),
+                        new Vector2D(Distances.LAUNCHPAD, Limelight.PAD_YAW.get()));
 }
