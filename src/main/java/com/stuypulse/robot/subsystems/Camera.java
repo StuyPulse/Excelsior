d1ff2c8589e03a3955a80d3922a9d68da1adbfad
Myles Pasetsky <34213819+selym3@users.noreply.github.com>
2022-07-13 18:56:44 -0400

    prepare for IRI  (#117)
    * Prepare code for shot map tuning
    
    * Add more shot map data
    
    * Put camera distance on dashboard
    
    * Start to remap shooter controllers
    
    Co-authored-by: Prog694 <prog692@gmail.com>

--- src/main/java/com/stuypulse/robot/subsystems/Camera.java
+++ src/main/java/com/stuypulse/robot/subsystems/Camera.java
@@ -14,6 +14,7 @@ import com.stuypulse.robot.constants.Settings;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.util.net.PortForwarder;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase {
@@ -87,6 +88,8 @@ public class Camera extends SubsystemBase {
 
     @Override
     public void periodic() {
+        SmartDashboard.putNumber("Camera/Distance", getDistance());
+
         if (!limelight.isConnected()) {
             Settings.reportWarning("Limelight Disconnected!");
         }
