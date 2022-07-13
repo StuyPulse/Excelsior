5323a09dd0ef0ccdc28278d105104273ac8b7897
Prog694 <prog692@gmail.com>
2022-07-13 12:01:37 -0400

    Prepare code for shot map tuning

--- src/main/java/com/stuypulse/robot/constants/ShotMap.java
+++ src/main/java/com/stuypulse/robot/constants/ShotMap.java
@@ -23,8 +23,12 @@ public interface ShotMap {
         public interface Distances{
                 double RING = Limelight.RING_DISTANCE.get();
                 double POINT_A = Units.inchesToMeters(167);
-                double POINT_B = Units.inchesToMeters(184);
+                double POINT_B = Units.inchesToMeters(176);
+                double POINT_C = Units.inchesToMeters(184);
+                double POINT_D = Units.inchesToMeters(193);
+                double POINT_E = Units.inchesToMeters(201);
                 double LAUNCHPAD = Limelight.PAD_DISTANCE.get();
+                double FAR = Units.inchesToMeters(234);
         }       
 
         // Converts a distance measurement to an RPM to shoot at
@@ -32,7 +36,10 @@ public interface ShotMap {
                 new NearestInterpolator(
                         new Vector2D(Distances.RING, Settings.Shooter.RING_RPM.get()),
                         new Vector2D(Distances.POINT_A, 3050),
-                        new Vector2D(Distances.POINT_B, 3200),    
+                        new Vector2D(Distances.POINT_B, 3200),
+                        new Vector2D(Distances.POINT_C, 3200),
+                        new Vector2D(Distances.POINT_D, 3200),
+                        new Vector2D(Distances.POINT_E, 3200),
                         new Vector2D(Distances.LAUNCHPAD, Settings.Shooter.PAD_RPM.get()));
 
         // Converts a distance measurement to an angle offset to align to
@@ -41,5 +48,8 @@ public interface ShotMap {
                         new Vector2D(Distances.RING, Limelight.RING_YAW.get()),
                         new Vector2D(Distances.POINT_A, 6.25),
                         new Vector2D(Distances.POINT_B, 5.70),
+                        new Vector2D(Distances.POINT_C, 5.70),
+                        new Vector2D(Distances.POINT_D, 5.70),
+                        new Vector2D(Distances.POINT_E, 5.70),
                         new Vector2D(Distances.LAUNCHPAD, Limelight.PAD_YAW.get()));
 }
