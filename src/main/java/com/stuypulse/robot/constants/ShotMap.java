8189641de1a0ff4f1e0d43567996d441001776b7
Prog694 <prog692@gmail.com>
2022-07-15 21:36:47 -0400

    Add POINT_C to ShotMap

--- src/main/java/com/stuypulse/robot/constants/ShotMap.java
+++ src/main/java/com/stuypulse/robot/constants/ShotMap.java
@@ -25,20 +25,17 @@ public interface ShotMap {
                 double RING = Limelight.RING_DISTANCE.get();
                 double POINT_A = Units.inchesToMeters(167);
                 double POINT_B = Units.inchesToMeters(184);
-                double POINT_D = Units.inchesToMeters(193);
-                double POINT_E = Units.inchesToMeters(201);
+                double POINT_C = Units.inchesToMeters(201);
                 double LAUNCHPAD = Limelight.PAD_DISTANCE.get();
-                double FAR = Units.inchesToMeters(234);
         }       
 
         // Converts a distance measurement to an RPM to shoot at
         Interpolator DISTANCE_TO_RPM =
                 new NearestInterpolator(
                         new Vector2D(Distances.RING, Settings.Shooter.RING_RPM.get()),
-                        new Vector2D(Distances.POINT_A, 3050),
-                        new Vector2D(Distances.POINT_B, 3200),
-                        new Vector2D(Distances.POINT_D, 3300),
-                        new Vector2D(Distances.POINT_E, 3400),
+                        new Vector2D(Distances.POINT_A, 3175),
+                        new Vector2D(Distances.POINT_B, 3325),
+                        new Vector2D(Distances.POINT_C, 3500),
                         new Vector2D(Distances.LAUNCHPAD, Settings.Shooter.PAD_RPM.get()));
 
         public static double getRPM(double distance) {
@@ -49,10 +46,9 @@ public interface ShotMap {
         Interpolator DISTANCE_TO_YAW =
                 new NearestInterpolator(
                         new Vector2D(Distances.RING, Limelight.RING_YAW.get()),
-                        new Vector2D(Distances.POINT_A, 6.25),
+                        new Vector2D(Distances.POINT_A, 5.1),
                         new Vector2D(Distances.POINT_B, 5.70),
-                        new Vector2D(Distances.POINT_D, 5.50),
-                        new Vector2D(Distances.POINT_E, 5.25),
+                        new Vector2D(Distances.POINT_C, 6.0),
                         new Vector2D(Distances.LAUNCHPAD, Limelight.PAD_YAW.get()));
         
         public static Angle getAngle(double distance) {
