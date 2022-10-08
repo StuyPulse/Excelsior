32954af2cde999b886154f37174b3f520aeabea3
carminvuong <95779419+carminvuong@users.noreply.github.com>
2022-10-08 15:05:02 -0400

    add more methods to IClimber

--- src/main/java/com/stuypulse/robot/subsystems/IClimber.java
+++ src/main/java/com/stuypulse/robot/subsystems/IClimber.java
@@ -7,6 +7,14 @@ public abstract class IClimber extends SubsystemBase{
         MAX_TILT, NO_TILT
     }
  
+    public abstract void setMotor();
+    public abstract void setMotorStop();
+
+    public abstract boolean getBottomHeightLimitReached();
+    public abstract boolean getTopHeightLimitReached();
+
+    public abstract void forceLowerClimber();
+    
     public abstract void setTilt(Tilt tilt);
     public abstract void setVoltage(double voltage);
 }
